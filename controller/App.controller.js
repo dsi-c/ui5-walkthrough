3e1f762b477fa25853b6ecb9f0b19948c0a68355
dsi-c <85294722+dsi-c@users.noreply.github.com>
2024-01-24 09:57:42 +0000

    #

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/m/library",
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, library) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -12,8 +13,13 @@ sap.ui.define(
         const MdlUser = this.getOwnerComponent().getModel("MdlUser");
         const dUser = MdlUser.getProperty("/user");
         const dLoggedIn = MdlUser.getProperty("/loggedin");
+        const yUser = MdlUser.getData();
+        console.log(yUser.loggedin);
         // show message
-        MessageToast.show(dLoggedIn);
+        //MessageToast.show(dLoggedIn);
+        MessageToast.show(yUser.user);
+
+        //library.URLHelper.redirect("https://www.office.com", false);
       },
     });
   },
