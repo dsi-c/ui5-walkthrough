728717a6d18822f7e209045a7033625290ee8881
dsi-c <85294722+dsi-c@users.noreply.github.com>
2024-01-24 08:47:52 +0000

    #

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,5 +1,20 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, MessageToast, JSONModel) => {
+    "use strict";
 
-  return Controller.extend("ui5.walkthrough.controller.App", {});
-});
+    return Controller.extend("ui5.walkthrough.controller.App", {
+      onInit() {
+        const MdlUser = this.getOwnerComponent().getModel("MdlUser");
+        const dUser = MdlUser.getProperty("/user");
+        const dLoggedIn = MdlUser.getProperty("/loggedin");
+        // show message
+        MessageToast.show(dLoggedIn);
+      },
+    });
+  },
+);
